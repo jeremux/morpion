1077532f4f122d04542659b3ae384c381e7c919e
Jeremy F <jeremy.fontaine@live.fr>
2016-09-04 03:26:27 +0200

    Commit
    Ajout structure Couple/Position

---
+++ src/main/java/morpion/entities/Couple.java
@@ -0,0 +1,88 @@
+package morpion.entities;
+
+/**
+ * Created by jeremux on 04/09/2016.
+ */
+public class Couple
+{
+    private int x;
+    private int y;
+    private int p1;
+    private int p2;
+
+    public int getX()
+    {
+        return x;
+    }
+    public void setX(int x)
+    {
+        this.x = x;
+    }
+    public int getY()
+    {
+        return y;
+    }
+    public void setY(int y)
+    {
+        this.y = y;
+    }
+
+    public Couple(int x, int y)
+    {
+        super();
+        this.x = x;
+        this.y = y;
+        this.p1 = -1;
+        this.p2 = -1;
+    }
+    public Couple()
+    {
+        // TODO Auto-generated constructor stub
+        super();
+    }
+    @Override
+    public int hashCode()
+    {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + x;
+        result = prime * result + y;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Couple couple = (Couple) o;
+
+        if (x != couple.x) return false;
+        if (y != couple.y) return false;
+        if (p1 != couple.p1) return false;
+        return p2 == couple.p2;
+
+    }
+
+    /**
+     * @return the position
+     */
+    public int getPositionX()
+    {
+        return p1;
+    }
+
+    public int getPositionY()
+    {
+        return p2;
+    }
+
+
+    public void setPositionX(int x) {
+        this.p1=x;
+    }
+
+    public void setPositionY(int y) {
+        this.p2=y;
+    }
+}
---
+++ src/test/java/morpion/entities/CoupleTest.java
@@ -0,0 +1,69 @@
+package morpion.entities;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by jeremux on 04/09/2016.
+ */
+public class CoupleTest extends TestCase
+{
+    public void setUp() throws Exception
+    {
+        super.setUp();
+
+
+    }
+
+    public void tearDown() throws Exception
+    {
+
+    }
+
+
+    public void testGetX() throws Exception
+    {
+        fail("not implemented");
+    }
+
+    public void testSetX() throws Exception
+    {
+        fail("not implemented");
+    }
+
+
+    public void testGetY() throws Exception
+    {
+        fail("not implemented");
+    }
+
+    public void testSetY() throws Exception
+    {
+        fail("not implemented");
+
+    }
+
+    public void testGetPositionX() throws Exception
+    {
+        fail("not implemented");
+
+    }
+
+    public void testGetPositionY() throws Exception
+    {
+        fail("not implemented");
+
+    }
+
+    public void testSetPositionX() throws Exception
+    {
+        fail("not implemented");
+
+    }
+
+    public void testSetPositionY() throws Exception
+    {
+        fail("not implemented");
+
+    }
+
+}
